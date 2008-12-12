43a58712a86588e12b767307b934c3d915745fd6
Eric Davis <edavis@littlestreamsoftware.com>
2008-12-12 14:46:22 -0800

    Added named scopes to NextIssue. #1552

--- app/models/next_issue.rb
+++ app/models/next_issue.rb
@@ -2,4 +2,21 @@ class NextIssue < ActiveRecord::Base
   belongs_to :issue
   belongs_to :user
   acts_as_list
+  
+  named_scope :doing_now, lambda { |user|
+    {
+      :conditions => { :user_id => user.id },
+      :limit => 5,
+      :order => 'position ASC'
+    }
+  }
+
+  named_scope :recommended, lambda { |user|
+    {
+      :conditions => { :user_id => user.id },
+      :limit => 10,
+      :offset => 5,
+      :order => 'position ASC'
+    }
+  }
 end
