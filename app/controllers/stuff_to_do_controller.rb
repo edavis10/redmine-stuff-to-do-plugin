de9d6d9678bb52a136ffd1d3847e770d930dc93d
Eric Davis <edavis@littlestreamsoftware.com>
2008-12-12 11:37:39 -0800

    Added NextIssue finder to the index for the doing now issues.  #1549

--- app/controllers/stuff_to_do_controller.rb
+++ app/controllers/stuff_to_do_controller.rb
@@ -1,4 +1,8 @@
 class StuffToDoController < ApplicationController
   def index
+    @doing_now = NextIssue.find(:all,
+                                :conditions => ['user_id = ?', User.current.id],
+                                :limit => 5,
+                                :order => 'position ASC')
   end
 end
--- spec/controllers/stuff_to_do_controller_spec.rb
+++ spec/controllers/stuff_to_do_controller_spec.rb
@@ -1,8 +1,29 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe StuffToDoController, '#index' do
+  before(:each) do
+    @current_user = mock_model(User, :admin? => false, :logged? => true)
+    User.stub!(:current).and_return(@current_user)
+  end
+  
   it 'should be successful' do
     get :index
     response.should be_success
   end
+  
+  it 'should render the index template' do
+    get :index
+    response.should render_template('index')
+  end
+  
+  it 'should set @doing_now to the top 5 issues for the current user' do
+    stuff = []
+    5.times { stuff << mock('stuff') }
+    NextIssue.should_receive(:find).with(:all, { :conditions => ['user_id = ?', @current_user.id], :limit => 5, :order => 'position ASC' }).and_return(stuff)
+    get :index
+    assigns[:doing_now].should have(5).things
+  end
+  
 end
+
+# TODO: Test unauthenticated
