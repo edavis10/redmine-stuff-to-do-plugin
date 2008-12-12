656b811d60369eba40b593b114b3e18645a3d16a
Eric Davis <edavis@littlestreamsoftware.com>
2008-12-12 12:32:23 -0800

    Added basic list of doing now tasks. #1549

--- features/step_definitions/what_im_doing_now_steps.rb
+++ features/step_definitions/what_im_doing_now_steps.rb
@@ -1,7 +1,35 @@
+# Mocha mocks
+
+Given /I am logged in/ do
+  @current_user = User.new(:mail => 'test@example.com', :firstname => 'Feature', :lastname => 'Test')
+  @current_user.login = 'feature_test'
+  @current_user.save!
+
+  User.stubs(:current).returns(@current_user)
+end
+
 Given /I am on the stuff to do page/ do
   visit "/stuff_to_do"
 end
 
+Given /there are (\d+) next issues/ do |number|
+  NextIssue.destroy_all
+  number.to_i.times do |n|
+    NextIssue.create! :user => @current_user
+  end
+end
+
+
 Then /^I should see a list of tasks called "doing-now"$/ do
   response.should have_tag("ul#doing-now")
 end
+
+Then /^I should see a row for each task to do now$/ do
+  response.should have_tag("li")
+end
+
+# TODO: Redmine needs so much built up, this test is unresponable (issue > project > custom fields, custom values, trackers)
+# Then /^I should see the issue title in the row$/ do
+#   response.should  have_tag("li", 'Issue 1 Title')
+# end
+
--- features/what_im_doing_now.feature
+++ features/what_im_doing_now.feature
@@ -4,8 +4,11 @@ Feature: What I'm doing now
   wants to have a prioritized list of their tasks to do now.
 
   Scenario: See a prioritized list of tasks
-    Given I am on the stuff to do page
+    Given I am logged in
+    And I am on the stuff to do page
+    And there are 5 next issues
     Then I should see "What I'm doing now"
     And I should see a list of tasks called "doing-now"
-
+    And I should see a row for each task to do now
+    And I should see the issue title in the row
 
