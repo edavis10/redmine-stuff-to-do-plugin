1ce26c49cbe7bf511a9cac297fe101e7d0adf314
Eric Davis <edavis@littlestreamsoftware.com>
2008-12-12 16:03:58 -0800

    Adding ruby-debug

--- features/support/env.rb
+++ features/support/env.rb
@@ -9,3 +9,5 @@ require 'webrat/rails'
 # Comment out the next two lines if you're not using RSpec's matchers (should / should_not) in your steps.
 require 'cucumber/rails/rspec'
 require 'webrat/rspec-rails'
+
+require 'ruby-debug'
