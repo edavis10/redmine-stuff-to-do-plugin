ffe76f754d24d41a7538edb12d6169844d480806
Eric Davis <edavis@littlestreamsoftware.com>
2009-01-07 15:45:26 -0800

    Moved custom JavaScript to the assets file so it can be cached on the client.

--- assets/javascripts/stuff-to-do.js
+++ assets/javascripts/stuff-to-do.js
@@ -1,2 +1,73 @@
+// TODO: JSUnit test this
 jQuery(document).ready(function(){
+    attachSortables();
+
+    jQuery("#user_id").change(function() {  jQuery("form#user_switch").submit();  });
+    jQuery("#ajax-indicator").ajaxStart(function(){ jQuery(this).show();  });
+    jQuery("#ajax-indicator").ajaxStop(function(){ jQuery(this).hide();  });
+
+    jQuery("#filter").change(function() {
+        jQuery.ajax({
+            type: "GET",
+            url: '/stuff_to_do/available_issues.js',
+            data: jQuery('#filter').serialize(),
+            success: function(response) {
+                jQuery('#available-pane').html(response);
+                attachSortables();
+            },
+            error: function(response) {
+                jQuery("div.error").html("Error filtering pane.  Please refresh the page.").show();
+            }});
+    });
+
 });
+
+function attachSortables() {
+    jQuery("#available").sortable({ 
+        connectWith: ["#doing-now", "#recommended"], 
+        placeholder: 'drop-accepted',
+        dropOnEmpty: true,
+        update : function (event, ui) {
+            if (jQuery('#available li.issue').length > 0) {
+                jQuery("#available li.empty-list").hide();
+            } else {
+                jQuery("#available li.empty-list").show();
+            }
+        }
+    }); 
+
+    jQuery("#doing-now").sortable({ 
+        connectWith: ["#available", "#recommended"], 
+        dropOnEmpty: true,
+        placeholder: 'drop-accepted',
+        update : function (event, ui) { saveOrder(ui); }
+    });  
+
+    jQuery("#recommended").sortable({ 
+        connectWith: ["#available", "#doing-now"],
+        dropOnEmpty: true,
+        placeholder: 'drop-accepted',
+        update : function (event, ui) { saveOrder(ui); }
+    });  
+
+}
+
+function saveOrder() {
+    data = 'user_id=' + user_id + '&' + jQuery("#doing-now").sortable('serialize') + '&' + jQuery("#recommended").sortable('serialize');
+    if (filter != null) {
+        data = data + '&filter=' + filter;
+    }
+    jQuery.ajax({
+        type: "POST",
+        url: '/stuff_to_do/reorder.js',
+        data: data,
+        success: function(response) {
+            jQuery('#panes').html(response);
+            attachSortables();
+        },
+        error: function(response) {
+            jQuery("div.error").html("Error saving lists.  Please refresh the page and try again.").show();
+        }});
+
+}
+
