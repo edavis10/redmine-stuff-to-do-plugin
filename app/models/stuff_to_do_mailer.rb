45a14d20cf5ad472b1c5d4d94445043a212e14f9
Your Name <pavel@pavlinux.ru>
2012-08-04 18:27:22 +0400

    fixes

--- app/models/stuff_to_do_mailer.rb
+++ app/models/stuff_to_do_mailer.rb
@@ -1,18 +1,22 @@
+#
+
 class StuffToDoMailer < Mailer
 
-	def recommended_below_threshold(user, number_of_next_items)
-
-		mail(:to => Setting.plugin_stuff_to_do_plugin['email_to'],
-		     :subject => "What's Recommended is below the threshold",
-		     :threshold => Setting.plugin_stuff_to_do_plugin['threshold'], 
-		     :count => number_of_next_items, 
-		     :user => user) do |format|
-			format.html { render :layout => 'recommended_below_threshold' }
-			format.text { render :layout => 'recommended_below_threshold' }
-		     end
-
+	default :to => Setting.plugin_stuff_to_do_plugin['email_to']
+	
+	def index
 	end
 
-	def index
+	def recommended_below_threshold(user, number_of_next_items)
+		@user = user
+		@number_of_next_items = number_of_next_items
+
+		mail (:subject   => "What's Recommended is below the threshold",
+		      :threshold => Setting.plugin_stuff_to_do_plugin['threshold'], 
+		      :count     => number_of_next_items, 
+		      :user      => user) do |format|
+			format.text
+			format.html 
+		      end
 	end
 end
