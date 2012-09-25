3a8886fce002e5a9658180fedb7794177830bb3f
David S Anderson <ande3577@gmail.com>
2012-09-25 18:23:04 -0500

    changed default enabled setting to true

--- db/migrate/007_add_enable_stuff_to_do_to_user.rb
+++ db/migrate/007_add_enable_stuff_to_do_to_user.rb
@@ -1,6 +1,6 @@
 class AddEnableStuffToDoToUser < ActiveRecord::Migration
   def self.up
-     add_column :user_preferences, :stuff_to_do_enabled, :boolean, :default => false
+     add_column :user_preferences, :stuff_to_do_enabled, :boolean, :default => true
   end
   
   def self.down
