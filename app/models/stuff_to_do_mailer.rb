c76bcaa678c82ea2a144a724943a743c0919a2f0
Yury Rumega <rumegauu@ugpa.ru>
2012-09-27 15:14:54 +0400

    Added code to support ActionMailer from Rails 3 (this fixes an 'Internal Server Error' when user closes his last issue)

--- app/models/stuff_to_do_mailer.rb
+++ app/models/stuff_to_do_mailer.rb
@@ -1,16 +1,30 @@
 class StuffToDoMailer < Mailer
   def recommended_below_threshold(user, number_of_next_items)
-    recipients Setting.plugin_stuff_to_do_plugin['email_to'].split(',')
-    subject "What's Recommended is below the threshold"
-    
-    body(
-         :threshold => Setting.plugin_stuff_to_do_plugin['threshold'],
-         :count => number_of_next_items,
-         :user => user
-         )
+    if Rails::VERSION::MAJOR >= 3
+        @to = Setting.plugin_stuff_to_do_plugin['email_to'].split(',')
+        @subject = "What's Recommended is below the threshold"
 
-    part :content_type => "text/plain", :body => render_message("recommended_below_threshold.erb", body)
-    part :content_type => "text/html", :body => render_message("recommended_below_threshold.text.html.erb", body)
+        @threshold = Setting.plugin_stuff_to_do_plugin['threshold']
+        @count = number_of_next_items
+        @user = user
 
+        mail(:to => @to, :subject => @subject) do |format|
+             format.text { render "recommended_below_threshold.erb" }
+             format.html { render "recommended_below_threshold.text.html.erb" }
+
+     else
+    	recipients Setting.plugin_stuff_to_do_plugin['email_to'].split(',')
+	subject "What's Recommended is below the threshold"
+
+    	body(
+             :threshold => Setting.plugin_stuff_to_do_plugin['threshold'],
+             :count => number_of_next_items,
+             :user => user
+             )
+
+        part :content_type => "text/plain", :body => render_message("recommended_below_threshold.erb", body)
+        part :content_type => "text/html", :body => render_message("recommended_below_threshold.text.html.erb", body)
+     end
+    end
   end
 end
