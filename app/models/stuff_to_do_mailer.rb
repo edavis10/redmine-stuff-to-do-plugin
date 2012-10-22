760c30ede9b9d1081c77ea53c3364bf43a36c31a
Steffen Schüssler <software@neffets.de>
2012-10-22 16:51:09 +0200

    refs #6551 - merge stuff of "Romega" - stuff_to_do_mailer is working in redmine 1.4 too, because the former sent the text part in a html-body-wrapped, I now use the Mailer function render_multipart. Added german translation. Missing translation of the text-Message, we have Settings.default_language de, but its sending only english text.

--- app/models/stuff_to_do_mailer.rb
+++ app/models/stuff_to_do_mailer.rb
@@ -1,26 +1,26 @@
 class StuffToDoMailer < Mailer
   def recommended_below_threshold(user, number_of_next_items)
+    set_language_if_valid Setting.default_language
     if Rails::VERSION::MAJOR >= 3
-        @to = Setting.plugin_stuff_to_do_plugin['email_to'].split(',')
-        @subject = "What's Recommended is below the threshold"
+      @to = Setting.plugin_stuff_to_do_plugin['email_to'].split(',')
+      @subject = "What's Recommended is below the threshold"
 
-        @threshold = Setting.plugin_stuff_to_do_plugin['threshold']
-        @count = number_of_next_items
-        @user = user
+      @threshold = Setting.plugin_stuff_to_do_plugin['threshold']
+      @count = number_of_next_items
+      @user = user
 
-        mail(:to => @to, :subject => @subject, :template_name => "recommended_below_threshold")
-     else
-        recipients Setting.plugin_stuff_to_do_plugin['email_to'].split(',')
-        subject "What's Recommended is below the threshold"
+      mail(:to => @to, :subject => @subject, :template_name => "recommended_below_threshold")
+    else
+      recipients Setting.plugin_stuff_to_do_plugin['email_to'].split(',')
+      subject "What's Recommended is below the threshold"
 
-        body(
-             :threshold => Setting.plugin_stuff_to_do_plugin['threshold'],
-             :count => number_of_next_items,
-             :user => user
-             )
+      body(
+        :threshold => Setting.plugin_stuff_to_do_plugin['threshold'],
+        :count => number_of_next_items,
+        :user => user
+      )
 
-        part :content_type => "text/plain", :body => render_message("recommended_below_threshold.text.erb", body)
-        part :content_type => "text/html", :body => render_message("recommended_below_threshold.html.erb", body)
+      render_multipart("recommended_below_threshold", body)
     end
   end
 end
