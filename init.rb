d5dc95d3e8dcc0cbcd4d20fc11d99819e4116df3
Kevin Neuenfeldt <kevin.neuenfeldt@googlemail.com>
2013-10-01 14:42:26 +0200

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :escalataion_alert do
+  name 'Escalataion Alert plugin'
+  author 'Kevin Neuenfeldt'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url ''
+  author_url ''
+end
