6a757948e4600024d56e5915d805c57c168cfa5b
Steve Schwartz <steve@alfajango.com>
2013-11-18 14:45:08 -0500

    Added periodic summary mailer for email configured for plugin email_to setting.

--- app/models/stuff_to_do_mailer.rb
+++ app/models/stuff_to_do_mailer.rb
@@ -33,4 +33,16 @@ class StuffToDoMailer < Mailer
            format.html
          end
   end
+
+  def periodic_summary(users_stuff_to_dos_hash)
+    @users_stuff_to_dos_hash = users_stuff_to_dos_hash
+    @bypass_user_allowed_to_view = true
+    mail(:to => Setting.plugin_stuff_to_do_plugin['email_to'],
+         :subject => "Stuff To Do Team Summary",
+         :bypass_user_allowed_to_view => @bypass_user_allowed_to_view,
+         :users_stuff_to_do_hash => @users_stuff_to_do_hash) do |format|
+           format.text
+           format.html
+         end
+  end
 end
