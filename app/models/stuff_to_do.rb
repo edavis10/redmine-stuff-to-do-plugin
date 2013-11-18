a802ea3cd475c612fc3d4cb7ed4e8ff7e260b1fc
Steve Schwartz <steve@alfajango.com>
2013-11-18 10:41:33 -0500

    Added statuses_for_stuff_to_do filter to listed issues in stuff to do page.

--- app/models/stuff_to_do.rb
+++ app/models/stuff_to_do.rb
@@ -251,6 +251,7 @@ class StuffToDo < ActiveRecord::Base
     scope = self
     #for Postgres:# conditions = "#{IssueStatus.table_name}.is_closed = false"
     conditions = "#{IssueStatus.table_name}.is_closed = false"
+    conditions << " AND (#{IssueStatus.table_name}.id IN (#{Setting.plugin_stuff_to_do_plugin['statuses_for_stuff_to_do'].join(',')}))"
     conditions << " AND (" << "#{Project.table_name}.status = %d" % [Project::STATUS_ACTIVE] << ")"
     conditions << " AND ((" << "assigned_to_id = %d" % [user.id] << ")"
     if(user.is_a?(User))
