f731ec9ebbcf937366f4c073140a0086fe616513
ande3577 <ande3577@gmail.com>
2013-12-15 09:13:51 -0600

    Set default mailer threshold to -1 (disabled), to prevent error until mailer can be fixed

--- init.rb
+++ init.rb
@@ -31,7 +31,7 @@ Redmine::Plugin.register :stuff_to_do_plugin do
   settings(:partial => 'settings/stuff_to_do_settings',
            :default => {
              'use_as_stuff_to_do' => '0',
-             'threshold' => '1',
+             'threshold' => '-1',
              'email_to' => 'example1@example.com,example2@example.com',
              'use_time_grid' => '0',
              'statuses_for_stuff_to_do' => ['all']
