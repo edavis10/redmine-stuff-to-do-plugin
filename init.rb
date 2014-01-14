69d4752a59604efdd48815c2f40a20dc83288e32
Kevin Neuenfeldt <kevin.neuenfeldt@googlemail.com>
2014-01-14 10:09:44 +0100

    Entered github url and raised version number

--- init.rb
+++ init.rb
@@ -2,8 +2,8 @@ Redmine::Plugin.register :redmine_reminder do
   name 'Redmine reminder'
   author 'Kevin Neuenfeldt'
   description 'This plugin can be used to automatically send emails to officers of issues which are about to expire.'
-  version '0.0.1'
-  url ''
+  version '0.1.0'
+  url 'https://github.com/raafael911/redmine_reminder.git'
   author_url ''
 
   settings :default => { :intervals => '' }, :partial => 'settings/redmine_reminder_settings'
