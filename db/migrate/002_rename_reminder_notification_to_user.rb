a03a4d36456089da43ed5c181cd770e1968fca40
Kevin Neuenfeldt <kevin.neuenfeldt@googlemail.com>
2014-01-15 14:44:30 +0100

    Removed call to update function in 2nd migration step

--- db/migrate/002_rename_reminder_notification_to_user.rb
+++ db/migrate/002_rename_reminder_notification_to_user.rb
@@ -1,9 +1,13 @@
 class RenameReminderNotificationToUser < ActiveRecord::Migration
   def up
-    c = CustomField.find_by_name_and_type('reminder_subscription', 'UserCustomField').update('Ticket-reminder Subscription')
+    c = CustomField.find_by_name_and_type('reminder_subscription', 'UserCustomField')
+    c.name = 'Ticket-reminder Subscription'
+    c.save
   end
 
   def down
-    c = CustomField.find_by_name_and_type('Ticket-reminder Subscription', 'UserCustomField').update(name: 'reminder_subscription')
+    c = CustomField.find_by_name_and_type('Ticket-reminder Subscription', 'UserCustomField')
+    c.name = 'reminder_subscription'
+    c.save
   end
 end
