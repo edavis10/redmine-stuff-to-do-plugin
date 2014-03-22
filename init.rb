15adc1b57c9d7c2d083da611d5e2e07f61a681db
Kevin Neuenfeldt <kevin.neuenfeldt@googlemail.com>
2014-03-22 18:02:23 +0100

    Updated README and description in init.rb

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ require_dependency 'hooks/view_hooks'
 Redmine::Plugin.register :redmine_reminder do
   name 'Redmine Reminder'
   author 'Kevin Neuenfeldt'
-  description 'This plugin can be used to automatically send emails to officers of issues which are about to expire.'
+  description 'This plugin can be used to automatically send emails to assignees of issues which are about to expire.'
   version '0.2.0'
   url 'https://github.com/raafael911/redmine_reminder.git'
   author_url ''
