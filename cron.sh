d6d0d124265d97e2c0ab2488402be7ac5bc6a39d
Kevin Neuenfeldt <kevin.neuenfeldt@googlemail.com>
2014-04-13 20:12:23 +0200

    Corrected cron.sh RAILS_ENV
    RAILS_ENV variable was not recognized by rails so it is not explicitly set in the rake command

--- cron.sh
+++ cron.sh
@@ -8,4 +8,4 @@ RAILS_ENV=production
 
 # These lines do the actual work
 cd $REDMINE_ROOT
-rake redmine:redmine_reminder:send_reminder_mails
+rake redmine:redmine_reminder:send_reminder_mails RAILS_ENV=$RAILS_ENV
