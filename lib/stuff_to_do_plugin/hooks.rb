4d838b7e1c0d63aae46f369acfc703ed90099546
Steffen Schüssler <neffets@neffets.de>
2017-02-17 11:35:41 +0100

    merge until ande3577/redmine-stuff-to-do-plugin/commit/95aa3b94d2c0deedfa6d8b76b0c9399dcb2f33a7 - Adds Permissions and User-Setting to enable/disable stuff-to-do-view

---
+++ lib/stuff_to_do_plugin/hooks.rb
@@ -0,0 +1,16 @@
+module MyPlugin
+  class Hooks < Redmine::Hook::ViewListener
+    # This just renders the partial in
+    # app/views/hooks/my_plugin/_view_issues_form_details_bottom.rhtml
+    # The contents of the context hash is made available as local variables to the partial.
+    #
+    # Additional context fields
+    #   :issue  => the issue this is edited
+    #   :f      => the form object to create additional fields
+    render_on :view_issues_show_description_bottom,
+          :partial => 'stuff_to_do/view_issues_show_description_bottom'
+    render_on :view_issues_context_menu_start,
+          :partial => 'stuff_to_do/view_issues_context_menu_start'
+    render_on :view_my_account, :partial => 'account_settings/stuff_to_do_account_settings', :layout => false
+  end
+end
