95c4062e61bd94c30d8774108c3455cfa6518b20
Steffen Schüssler <steffen.schuessler@berlinonline.de>
2017-03-24 00:21:59 +0100

    start for test cases

---
+++ test/unit/stuff_to_do_test.rb
@@ -0,0 +1,10 @@
+require File.expand_path('../../test_helper', __FILE__)
+
+class StuffToDoTest < ActiveSupport::TestCase
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
+
